98bfc79bc4ef670695b83dcdc457aef1c5baf35f
andrew gimma <andrewgimma@protonmail.com>
2023-05-25 12:46:06 -0400

    format

--- lib/bank.ex
+++ lib/bank.ex
@@ -120,10 +120,8 @@ defmodule Bank do
       when is_binary(from_user) and is_binary(to_user) and is_number(amount) and
              is_binary(currency) do
     case {enough_in_balance?(from_user, amount, currency),
-          enough_in_balance?(to_user, amount, currency),
-          user_exists?(from_user),
-          user_exists?(to_user)
-          } do
+          enough_in_balance?(to_user, amount, currency), user_exists?(from_user),
+          user_exists?(to_user)} do
       {_, _, false, _} ->
         {:error, :sender_does_not_exist}
 
