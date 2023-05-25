4b6446962b0ee45e1acb5d4f91e3cadc4cc9a537
andrew gimma <andrewgimma@protonmail.com>
2023-05-24 20:07:53 -0400

    send

--- lib/bank.ex
+++ lib/bank.ex
@@ -85,6 +85,12 @@ defmodule Bank do
     end
   end
 
+  @spec send(from_user :: String.t, to_user :: String.t, amount :: number, currency :: String.t) :: {:ok, from_user_balance :: number, to_user_balance :: number} | {:error, :wrong_arguments | :not_enough_money | :sender_does_not_exist | :receiver_does_not_exist | :too_many_requests_to_sender | :too_many_requests_to_receiver}
+  def send(from_user, to_user, amount, currency) do
+    withdraw(from_user, amount, currency)
+    deposit(to_user, amount, currency)
+  end
+
   ## GenServer Callbacks
 
   @impl true
