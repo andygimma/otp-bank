6dc0c9a6c1dd01e932c8842cec842ec5d3d8ed2f
Andy Gimma <andrewgimma@protonmail.com>
2023-05-26 19:09:45 -0700

    add tests

--- test/bank_test.exs
+++ test/bank_test.exs
@@ -2,7 +2,28 @@ defmodule BankTest do
   use ExUnit.Case
   doctest Bank
 
-  test "greets the world" do
-    assert Bank.hello() == :world
+  describe "OTP" do
+    test "the server starts" do
+      assert {:ok, _pid} = Bank.Server.start
+    end
+
+    test "a newly created user has expected state" do
+      Bank.Server.start
+      {:ok, pid} = Bank.create_user "user"
+      assert %{balance: 0.0, default_currency: "USD", id: nil} = :sys.get_state pid
+    end
   end
+
+  describe "Public API" do
+    test "create a user happy path" do
+      Bank.Server.start
+      assert {:ok, _pid} = Bank.create_user "user"
+    end
+
+    test "create a user with bad args" do
+      Bank.Server.start
+      assert {:error, :wrong_arguments} = Bank.create_user 12345
+    end
+  end
+
 end
